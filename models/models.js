e9162319128896ee2797a7e2388e8ddfe84cc6e1
necutli <necutliatzin@hotmail.com>
2015-07-15 14:05:24 -0500

    Multi pregunta

--- models/models.js
+++ models/models.js
@@ -37,6 +37,9 @@ sequelize.sync().then(function() {
 		if(count === 0) { // la tabla se inicializa solo si esta vacía
 		Quiz.create({ pregunta: 'Capital de Italia',
 						respuesta: 'Roma'
+						});
+		Quiz.create({ pregunta: 'Capital de Portugal',
+						respuesta: 'Lisboa'
 						})
 				.then(function(){console.log('Base de datos inicializada')});
 		};
