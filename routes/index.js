4f6a152a3adc20e6dd2204761e22eb278231848a
necutli <necutliatzin@hotmail.com>
2015-07-22 23:10:46 -0500

    Correcciones de Crear pregunta

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,6 @@ router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 router.get('/quizes/new',  quizController.new);
-router.get('/quizes/create',  quizController.create);
+router.post('/quizes/create',  quizController.create);
 
 module.exports = router;
