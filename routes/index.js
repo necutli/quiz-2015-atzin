67153d544d6e19d572b2b208a6494ad2af6b5556
necutli <necutliatzin@hotmail.com>
2015-08-21 01:16:02 -0500

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 router.get('/login', sessionController.new);  // cargar formulario login 
 router.post('/login',sessionController.create); //crear una nueva sesión
@@ -29,7 +30,10 @@ router.get('/quizes/:quizId(\\d+)/edit',  sessionController.loginRequired,  quiz
 router.put('/quizes/:quizId(\\d+)',       sessionController.loginRequired,  quizController.update);
 router.delete('/quizes/:quizId(\\d+)',    sessionController.loginRequired,  quizController.destroy);
 
+//rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, 
+																		commentController.publish );
 
 module.exports = router;
